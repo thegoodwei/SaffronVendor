78f5f5fdc2472afe8593e3f8f24972da4c65d573
Wade <94668210+thegoodwei@users.noreply.github.com>
2022-12-17 18:33:02 -0600

    Comment what it does

--- SaffronVendor.sol
+++ SaffronVendor.sol
@@ -21,6 +21,7 @@ contract SaffronVendor {
 
   event Bought(uint256 countBuys);
   event Redeem(uint256 redeemSaffron, uint256 paymentAmount);
+//buyAffron function receives payment in ETH and sends back 1 $affron token to the caller. The function also increments the paymentReceived variable and emits a Bought event with the current value of paymentReceived.
 
   function buyAffron() public payable {
     require(msg.value > 0, "Payment must be greater than zero");
@@ -28,6 +29,7 @@ contract SaffronVendor {
     paymentReceived = paymentReceived.add(msg.value);
     emit Bought(paymentReceived);
   }
+//redeemAffron function allows the contract owner to redeem $affron tokens and burn them. The function first checks that the contract has at least 1 $affron token to redeem, then transfers 1 $affron token to the caller and burns it. The function also emits a Redeem event with the current balance of $affron tokens on the contract and the total paymentReceived value.
 
   function redeemAffron() public {
     require(affron.balanceOf(address(this)) > 0, "There are no Saffron tokens to redeem");
