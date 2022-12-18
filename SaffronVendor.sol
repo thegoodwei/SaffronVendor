990da33ef564af98bfdb57bc5c2741085d6e79c3
Wade <94668210+thegoodwei@users.noreply.github.com>
2022-12-17 19:03:50 -0600

    Update SaffronVendor.sol

--- SaffronVendor.sol
+++ SaffronVendor.sol
@@ -12,7 +12,10 @@ contract SaffronVendor {
   uint8 public decimals = 18;
   uint256 public totalSupply = 10000;
   IERC20 public affron;
-  uint256 public paymentReceived = 0;
+
+uint256 public balance;
+  bool public functionExecuted;
+
 
   constructor() public {
     affron = new IERC20(this);
@@ -31,11 +34,17 @@ contract SaffronVendor {
   }
 //redeemAffron function allows the contract owner to redeem $affron tokens and burn them. The function first checks that the contract has at least 1 $affron token to redeem, then transfers 1 $affron token to the caller and burns it. The function also emits a Redeem event with the current balance of $affron tokens on the contract and the total paymentReceived value.
 
-  function redeemAffron() public {
-    require(affron.balanceOf(address(this)) > 0, "There are no Saffron tokens to redeem");
-    require(affron.transfer(msg.sender, 1), "Redeem failed");
-    affron.burn(1);
-    emit Redeem(affron.balanceOf(address(this)), paymentReceived);
-    paymentReceived = 0;
+ function redeemAffron(uint256 x) public {
+    require(x > 2, "Invalid amount");
+    require(affron.transferFrom(msg.sender, address(this), x), "Transfer failed");
+    // Call Rust function and get result
+    if (result) {
+      affron.burn(x);
+      balance = balance.sub(x);
+      functionExecuted = true;
+    } else {
+      functionExecuted = false;
+    }
+  }
   }
 }
