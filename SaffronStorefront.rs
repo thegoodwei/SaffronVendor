984cb421e3f0209d4b412ce0048a865964c3b957
Wade <94668210+thegoodwei@users.noreply.github.com>
2022-12-22 10:48:54 -0600

    Buy, redeem, Interface.
    This is the front end functions each separately functional

--- SaffronStorefront.rs
+++ SaffronStorefront.rs
@@ -1,3 +1,143 @@
+//FRONTEND INIT BUY REDEEM ON ETHEREUM
+// Import the Web3 library
+import Web3 from "web3";
+
+// Define the ABI (Application Binary Interface) of the smart contract
+// This is used to generate the contract object and interact with its functions
+const abi = [
+    // The "buy" function is a payable function that takes no inputs and returns no outputs
+    {
+        "inputs": [], // No input parameters
+        "name": "buy", // Function name
+        "outputs": [], // No output parameters
+        "stateMutability": "payable", // The function is payable (can receive Ether)
+        "type": "function", // The function is a smart contract function
+    },
+    // The "redeem" function is a nonpayable function that takes two bytes32 inputs (name and address) and returns no outputs
+    {
+        "inputs": [
+            {
+                "name": "name", // Name of the first input parameter
+                "type": "bytes" // The type of the first input parameter is bytes
+            },
+            {
+                "name": "address", // Name of the second input parameter
+                "type": "bytes" // The type of the second input parameter is bytes
+            }
+        ],
+        "name": "redeem", // Function name
+        "outputs": [], // No output parameters
+        "stateMutability": "nonpayable", // The function is nonpayable (cannot receive Ether)
+        "type": "function", // The function is a smart contract function
+    },
+    // ... other contract functions
+];
+
+// Define the contract address
+const contract_address = "0x...";
+
+// Initialize the Web3 provider
+// This provider allows us to send Ethereum transactions and call smart contract functions
+const web3 = new Web3(window.ethereum);
+
+// Define the buy function
+// Define the buy function (continued)
+async function buy() {
+    // Get the user's Ethereum address
+    const address = (await web3.eth.getAccounts())[0];
+
+    // Load the contract using the ABI and contract address
+    // This generates a contract object that we can use to interact with the contract's functions
+    const contract = new web3.eth.Contract(abi, contract_address);
+
+    // Call the contract's "buy" function
+    // This sends a transaction to the Ethereum network to call the function
+    // The "from" parameter specifies the sender address
+    await contract.methods.buy().send({ from: address });
+}
+
+// Define the redeem function
+/* This function takes in two string arguments: name and address. These represent the name and delivery address of the user who is redeeming saffron.
+
+The function first gets the Ethereum address of the user who is currently logged in to their wallet. This is done using the web3.eth.getAccounts method.
+
+Next, the function creates a contract object using the ABI and contract address that were defined earlier. This object allows us to interact with the functions of the smart contract. */
+async function redeem(name: String, address: String) {
+    // Get the user's Ethereum address
+    const sender = await web3.eth.getAccounts();
+
+    // Create a contract object using the ABI and contract address
+    const contract = new web3.eth.Contract(abi, contract_address);
+
+    // Convert the name and address strings to byte arrays
+    const name_bytes = web3.utils.fromAscii(name);
+    const address_bytes = web3.utils.fromAscii(address);
+
+    // Call the contract's "redeem" function
+    // This sends a transaction to the Ethereum network to call the function
+    // The "from" parameter specifies the sender address
+    // The "name" and "address" parameters are the inputs to the function
+    await contract.methods.redeem(name_bytes, address_bytes).send({ from: sender });
+}
+
+
+   
+// BUTTONS FOR BUY AND REDEEM
+/*
+<!-- Import the Web3 library -->
+<script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
+
+<!-- Define the buy and redeem functions -->
+<script>
+  async function buy() {
+    // Get the user's Ethereum address
+    const sender = await web3.eth.getAccounts();
+
+    // Create a contract object using the ABI and contract address
+    const contract = new web3.eth.Contract(abi, contract_address);
+
+    // Call the contract's "buy" function
+    // This sends a transaction to the Ethereum network to call the function
+    // The "from" parameter specifies the sender address
+    await contract.methods.buy().send({ from: sender });
+  }
+
+  async function redeem(name: String, address: String) {
+    // Get the user's Ethereum address
+    const sender = await web3.eth.getAccounts();
+
+    // Create a contract object using the ABI and contract address
+    const contract = new web3.eth.Contract(abi, contract_address);
+
+    // Convert the name and address strings to byte arrays
+    const name_bytes = web3.utils.fromAscii(name);
+    const address_bytes = web3.utils.fromAscii(address);
+
+    // Call the contract's "redeem" function
+    // This sends a transaction to the Ethereum network to call the function
+    // The "from" parameter specifies the sender address
+    // The "name" and "address" parameters are the inputs to the function
+    await contract.methods.redeem(name_bytes, address_bytes).send({ from: sender });
+  }
+</script>
+
+<!-- Define the buy button -->
+<button onclick="buy()">Buy</button>
+
+<!-- Define the redeem button -->
+<button onclick="redeem('John Smith', '123 Main St')">Redeem</button>
+
+
+
+
+
+
+
+
+
+
+//VISUAL INTERFACE 
+
 /*This is the complete Rust code for the visually sleek Yew webpage with a UX for a saffron vendor. This webpage has two buttons: the green buy button opens a lightbox to reveal a price / available dashboard, a secure input for a Quantity, and 'confirm'; The red redeem button opens a different lightbox and another dashboard, with secure input for Name, Address, and Quantity. */
 use yew::{html, Html, Component, ComponentLink, ShouldRender};
 use yew::services::console::consoleService;
